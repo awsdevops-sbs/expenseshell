71e29e22622d77b9d3343a9dd00b8c759b024794
akram <akram.khan200@gmail.com>
2025-12-12 18:48:31 +0530

    108

--- frontend.sh
+++ frontend.sh
@@ -3,10 +3,9 @@
     systemctl enable nginx
     systemctl start nginx
 cd /usr/share/nginx/html
+
     rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-frontend-v2.zip
-
-
 unzip /tmp/frontend.zip
 
 cp expense.conf /etc/nginx/default.d/expense.conf
