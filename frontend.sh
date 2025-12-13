a6440c700d1b7df7fc2457ec9d0603a2ba68ae1c
akram <akram.khan200@gmail.com>
2025-12-14 00:28:36 +0530

    1226

--- frontend.sh
+++ frontend.sh
@@ -6,16 +6,21 @@ Print_Task_Heading "Install Nginx"
 dnf install nginx -y &>>$Log
 check_status $?
 
+
+Print_Task_Heading "Copy Expense config "
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$Log
+check_status $?
+
+app_pre
+
 Print_Task_Heading "Start Nginx service"
  systemctl enable nginx &>>$Log
  systemctl start nginx &>>$Log
 check_status $?
 
-app_pre
 
-Print_Task_Heading "Copy Expense config "
-cp expense.conf /etc/nginx/default.d/expense.conf &>>$Log
-check_status $?
+
+
 
 
 Print_Task_Heading "Restart nginx"
